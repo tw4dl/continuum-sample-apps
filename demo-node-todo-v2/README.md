140feaf60073d755b558c306c0849ce2c660c0c9
George Haws <george@apcera.com>
2015-03-17 09:09:08 -0700

    todo demo v2

---
+++ demo-node-todo-v2/README.md
@@ -0,0 +1,3 @@
+### demo-node-todo
+
+This app is used as an example in the Continuum Tutorial, and includes functionality for connecting to Postgres and MySQL databases. It based on [TodoMVC](http://todomvc.com/).
---
+++ demo-node-todo-v2/app.js
@@ -0,0 +1,83 @@
+var express   = require('express'),
+    bodyParser = require('body-parser'),
+    https     = require('https'),
+    http      = require('http'),
+    routes    = require('./routes'),
+    routes_pg = require('./lib/routes_pg'),
+    routes_ms = require('./lib/routes_ms'),
+    uuid      = require('node-uuid');
+
+// misc config
+var app = module.exports = express();
+
+// Express config
+app.use(bodyParser.json());
+app.use(function(req, res, next){
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+var port = process.env.PORT || 5001;
+var server = app.listen(port, function() {
+  console.log('listening on port ', port);
+});
+
+
+// Express routes
+app.get('/', routes.index);
+app.get('/client.js', routes.client);
+app.get('/angular.min.js', routes.angular);
+app.get('/angular-route.min.js', routes.angularRoute);
+app.get('/jquery.min.js', routes.jquery);
+app.get('/todoCtrl.js', routes.todoctrl);
+app.get('/todoBlur.js', routes.todoblur);
+app.get('/todoFocus.js', routes.todofocus);
+app.get('/todoStorage.js', routes.todostorage);
+app.get('/base.css', routes.base);
+app.get('/bg.png', routes.bg);
+app.get('/logo.jpg', routes.logo);
+app.get('/app.css', routes.appcss);
+app.get('/quit', function(req, res) {
+  process.exit(1);
+});
+
+app.get('/env', routes.env);
+
+var uuid = uuid.v1();
+var startTime = new Date();
+
+app.get('/instance', function(req, res) {
+  console.log('hello!')
+  console.log(req.host)
+  res.send({
+    instance_id: uuid,
+    start_time: startTime,
+    host: req.host
+  });
+});
+
+
+// Postgres routes
+if (typeof routes_pg.createTables !== "undefined") {
+  app.get('/createPGTables', routes_pg.createTables);
+  app.get('/showPGTables', routes_pg.showTables);
+  app.get('/showPGTasks', routes_pg.showTasks);
+  app.get('/deletePGTables', routes_pg.deleteTables);
+  app.post('/addPGTask', routes_pg.addTask);
+  app.get('/deletePGRows', routes_pg.deleteRows);
+  app.get('/pg_conn_info', routes_pg.getConnInfo);
+  app.get('/pg_conn_status/:user/:passwd', routes_pg.updateConnStatus);
+  app.get('/pg_conn_status', routes_pg.getConnStatus);
+}
+
+// MySQL routes
+if (typeof routes_ms.createTables !== "undefined") {
+  app.get('/createMSTables', routes_ms.createTables);
+  app.get('/showMSTables', routes_ms.showTables);
+  app.get('/showMSTasks', routes_ms.showTasks);
+  app.get('/deleteMSTables', routes_ms.deleteTables);
+  app.post('/addMSTask', routes_ms.addTask);
+  app.get('/deleteMSRows', routes_ms.deleteRows);
+  app.get('/ms_conn_info', routes_ms.getConnInfo);
+  app.get('/ms_conn_status/:user/:passwd', routes_ms.updateConnStatus);
+  app.get('/ms_conn_status', routes_ms.getConnStatus);
+}
